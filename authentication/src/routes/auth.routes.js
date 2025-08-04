31e4047fc6d48d52696f05800ad2e4a93de1cd10
Satyendra Singh <deepaknaruka2411@gmail.com>
2025-08-04 18:40:48 +0530

    Added JWT authentication and cookie handling to user registration and login

--- authentication/src/routes/auth.routes.js
+++ authentication/src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const userModel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
@@ -7,6 +8,10 @@ router.post("/register", async (req, res) => {
   const { username, password } = req.body;
 
   const user = await userModel.create({ username, password });
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+  res.cookie("token", token)
 
   res.status(201).json({
     message: "User registered successfully",
@@ -29,15 +34,39 @@ router.post("/login", async (req, res) => {
 
   let isPasswordValid = password === user.password;
 
-  if(!isPasswordValid){
+  if (!isPasswordValid) {
     return res.status(401).json({
-        message: "Invalid password"
-    })
+      message: "Invalid password",
+    });
   }
 
   res.status(200).json({
     message: "User logged in successfully",
-  })
+  });
+});
+
+router.get("/user", async (req, res) => {
+  const { token } = req.cookies;
+
+  if (!token) {
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await userModel.findById(decoded.id).select("-password -__v");
+
+    return res.status(200).json({
+      message: "User fetched successfully",
+      user,
+    });
+  } catch (err) {
+    return res.status(401).json({
+      message: "Unauthorized - Invalid token",
+    });
+  }
 });
 
 module.exports = router;
