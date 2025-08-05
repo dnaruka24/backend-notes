b800b5e9fb679add24b00ad1c5736507e0804c9f
Satyendra Singh <deepaknaruka2411@gmail.com>
2025-08-05 13:19:02 +0530

    Refactor authentication routes to improve user registration and login flow

--- authentication/src/routes/auth.routes.js
+++ authentication/src/routes/auth.routes.js
@@ -5,43 +5,25 @@ const jwt = require("jsonwebtoken");
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
-  const { username, password } = req.body;
+  const { username, password } = req.body;  
+
+  const existingUser = await userModel.findOne({ username });
+
+  if (existingUser) {
+    return res.status(409).json({
+      message: "User already exists",
+    });
+  }
 
   const user = await userModel.create({ username, password });
 
   const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
 
-  res.cookie("token", token)
+  res.cookie("token", token);
 
   res.status(201).json({
     message: "User registered successfully",
     user,
-  });
-});
-
-router.post("/login", async (req, res) => {
-  const { username, password } = req.body;
-
-  let user = await userModel.findOne({
-    username: username,
-  });
-
-  if (!user) {
-    return res.status(401).json({
-      message: "User not found | Invalid username",
-    });
-  }
-
-  let isPasswordValid = password === user.password;
-
-  if (!isPasswordValid) {
-    return res.status(401).json({
-      message: "Invalid password",
-    });
-  }
-
-  res.status(200).json({
-    message: "User logged in successfully",
   });
 });
 
@@ -68,5 +50,42 @@ router.get("/user", async (req, res) => {
     });
   }
 });
+
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  let user = await userModel.findOne({
+    username: username,
+  });
+
+  if (!user) {
+    return res.status(401).json({
+      message: "User not found | Invalid username",
+    });
+  }
+
+  let isPasswordValid = password === user.password;
+
+  if (!isPasswordValid) {
+    return res.status(401).json({
+      message: "Invalid password",
+    });
+  }
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+  res.cookie("token", token);
+
+  res.status(200).json({
+    message: "User logged in successfully",
+  });
+});
+
+router.get("/logout", (req, res) => {
+  res.clearCookie("token");
+  res.status(200).json({
+    message: "User logged out successfully",
+  });
+})
 
 module.exports = router;
