c1b4b642ddbd4c070f2e1def05d1f69cbc4aa100
Satyendra Singh <deepaknaruka2411@gmail.com>
2025-08-11 18:32:05 +0530

    Created protected API by using middleware

---
+++ project-Caption_Generator/src/routes/post.routes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const authMiddleware = require("../middlewares/auth.middleware")
+
+const router = express.Router();
+
+router.post("/", authMiddleware);
+
+module.exports = router;
