fb0419643aa13a63b717b7c5ed8a5c37fc567b28
Satyendra Singh <deepaknaruka2411@gmail.com>
2025-08-23 23:14:17 +0530

    Added authentication middleware and update chat route

--- project-2/src/app.js
+++ project-2/src/app.js
@@ -1,9 +1,16 @@
 const express = require("express");
-const authRoutes = require("./routes/auth.route")
+const cookieParser = require("cookie-parser");
+const authRoutes = require("./routes/auth.route");
+const chatRoutes = require("./routes/chat.route")
 
 const app = express();
-app.use(express.json());
 
+/* using middlewares*/
+app.use(express.json());
+app.use(cookieParser())
+
+/* using routes */
 app.use("/api/auth", authRoutes)
+app.use("/api/chat", chatRoutes)
 
 module.exports = app;
