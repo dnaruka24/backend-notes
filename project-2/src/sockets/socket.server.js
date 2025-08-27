9dff73d51b34cf12a87b74cbbe89c6b2b2308888
Satyendra Singh <deepaknaruka2411@gmail.com>
2025-08-27 12:55:47 +0530

    Add cookie dependency and implement authentication middleware for socket connections

--- project-2/src/sockets/socket.server.js
+++ project-2/src/sockets/socket.server.js
@@ -1,10 +1,32 @@
 const { Server } = require("socket.io");
+const cookie = require("cookie");
+const jwt = require("jsonwebtoken");
+const userModel = require("../model/user.model");
 
-function socketServer(httpServer){
+function socketServer(httpServer) {
     const io = new Server(httpServer, {})
 
-    io.on("connection", (socket)=>{
-        console.log("New socket connected", socket.id)})
+    io.use(async (socket, next) => {
+        const cookies = cookie.parse(socket.handshake.headers?.cookie || "");
+        if (!cookies.token) {
+            return next(new Error("Authentication error: No token provided"));
+        }
+
+        try {
+            const decoded = jwt.verify(cookies.token, process.env.JWT_SECRET);
+            const user = await userModel.findById(decoded.id);
+            socket.user = user;
+            next();
+        } catch (error) {
+            return next(new Error("Authentication error: Invalid token"));
+        }
+    })
+
+    io.on("connection", (socket) => {
+        socket.on("ai-message", (messagePayload) => {
+            console.log("Received ai-message:", messagePayload);
+        })
+    })
 }
 
 module.exports = socketServer;
