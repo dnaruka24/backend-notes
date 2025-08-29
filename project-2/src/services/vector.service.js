c5aa0f0c1be254125d400d93241f25f40d7fcef2
Satyendra Singh <deepaknaruka2411@gmail.com>
2025-08-29 22:05:41 +0530

    Add Pinecone database integration to project dependencies and create vector service

---
+++ project-2/src/services/vector.service.js
@@ -0,0 +1,6 @@
+// Import the Pinecone library
+import { Pinecone } from '@pinecone-database/pinecone'
+
+// Initialize a Pinecone client with your API key
+const pc = new Pinecone({ apiKey: process.env.PINECONE_API_KEY });
+
